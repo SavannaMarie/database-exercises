f5628cc31a2f633cd346c0b0f058b2325faf032c
Savanna Marie <savannamarie@gmail.com>
2021-06-17 16:34:25 -0500

    MySQL intro

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,1 +1,12 @@
 USE codeup_test_db;
+DROP TABLE IF EXISTS albums;
+CREATE TABLE albums (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(50) NOT NULL,
+    name VARCHAR(70) NOT NULL,
+    release_date INT UNSIGNED,
+    sales FLOAT,
+    genre VARCHAR(50),
+    PRIMARY KEY (id)
+    );
+DESCRIBE albums;
