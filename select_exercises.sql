c0506fdd2fd81056ab1cf6122a6f2a17b5707706
Savanna Marie <savannamarie@gmail.com>
2021-06-18 12:05:26 -0500

    Select Exercise refactoring

--- select_exercises.sql
+++ select_exercises.sql
@@ -3,17 +3,17 @@ USE codeup_test_db;
 SELECT 'All Pink Floyd albums' AS 'Album Name';
 SELECT * FROM albums WHERE artist = 'Pink Floyd';
 
-SELECT 'The year Sgt. Pepper''s Lonely Hearts Club Band was released' AS 'Album Info';
-SELECT release_date FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+SELECT release_date AS 'Sgt. Pepper''s Lonely Hearts Club Band release date'
+FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
 
-SELECT 'The genre for Come On Over' AS 'Album Info';
-SELECT genre FROM albums WHERE name = 'Come On Over';
+SELECT genre AS 'The genre for Come On Over'
+FROM albums WHERE name = 'Come On Over';
 
-SELECT 'Which albums were released in the 1990s' AS 'Album Info';
-SELECT name FROM albums WHERE release_date BETWEEN 1990 AND 2000;
+SELECT name AS 'Which albums were released in the 1990s'
+FROM albums WHERE release_date BETWEEN 1990 AND 2000;
 
-SELECT 'Which albums had less than 20 million certified sales' AS 'Album Info';
-SELECT name FROM albums WHERE sales < 20;
+SELECT name AS 'Which albums had less than 20 million certified sales'
+FROM albums WHERE sales < 20;
 
-SELECT 'All the albums with a genre of "Rock".' AS 'Album Info';
-SELECT name FROM albums WHERE genre = 'Rock';
+SELECT name AS 'All the albums with a genre of "Rock".'
+FROM albums WHERE genre = 'Rock';
