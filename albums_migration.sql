713e204685c9a4aea94c6709a5c91129cfb56d69
Savanna Marie <savannamarie@gmail.com>
2021-06-18 10:12:20 -0500

    SQL Exercises

--- albums_migration.sql
+++ albums_migration.sql
@@ -5,8 +5,11 @@ CREATE TABLE albums (
     artist VARCHAR(50) NOT NULL,
     name VARCHAR(70) NOT NULL,
     release_date INT UNSIGNED,
-    sales FLOAT,
     genre VARCHAR(50),
+    sales FLOAT,
     PRIMARY KEY (id)
     );
 DESCRIBE albums;
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Savanna','Savanna',1998,102,'Symphony')
