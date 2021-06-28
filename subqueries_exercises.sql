8dc166a70ec0ff4e4937b5bdca89f8e4d93cab78
Savanna Marie <savannamarie@gmail.com>
2021-06-28 10:45:58 -0500

    subqueries

---
+++ subqueries_exercises.sql
@@ -0,0 +1,35 @@
+USE employees;
+
+SELECT hire_date FROM employees WHERE emp_no = 101010;
+
+SELECT *
+FROM employees
+WHERE hire_date IN (
+    SELECT hire_date FROM employees WHERE emp_no = 101010
+);
+
+SELECT DISTINCT title
+FROM titles
+WHERE emp_no IN (
+    SELECT emp_no FROM employees WHERE first_name = 'Aamod'
+);
+
+SELECT first_name, last_name, birth_date
+FROM employees
+WHERE emp_no IN (
+    SELECT emp_no
+    FROM dept_manager
+    WHERE to_date = '9999-01-01'
+) AND gender = 'F';
+
+SELECT dept_name
+FROM departments
+WHERE dept_no IN (
+    SELECT dept_no
+    FROM dept_manager
+    WHERE emp_no IN (
+        SELECT emp_no
+        FROM employees
+        WHERE gender = 'F')
+      AND to_date > NOW()
+);
